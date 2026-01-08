63ebc2bcf2958c5055e864e121f381ab5d3dbb48
Tang Piseth <tangpiseth43@gmail.com>
2026-01-08 16:24:24 +1030

    A: remove

--- time_server.py
+++ time_server.py
@@ -4,7 +4,7 @@ import datetime
 print("<html><body>")
 print("<h1>Dynamic Python Page</h1>")
 print(f"<p>Server Time: {datetime.datetime.now()}</p>")
-print("</body></html>")`
+print("</body></html>")
 
 
 # https://gemini.google.com/app/8c7a37e6a87a0bba
